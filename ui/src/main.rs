c7208a7a75e7655bab3d5c966838cef8e2d2e563
Patrick Elsen <pelsen@xfbs.net>
2023-07-24 23:27:14 +0200

    Adds animations for navbar buttons

--- ui/src/main.rs
+++ ui/src/main.rs
@@ -16,15 +16,15 @@ fn NavBar() -> Html {
     html! {
         <nav class="bg-gray-200 border-gray-200 dark:bg-gray-900 sticky top-0 z-10 border">
             <div class="max-w-screen-4xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="/" class="flex items-center">
-                    <img src="/cindy.svg" class="h-8 mr-3" alt="Flowbite Logo" />
+                <a href="/" class="flex items-center group">
+                    <img src="/cindy.svg" class="h-8 mr-3 group-hover:scale-110 transition duration-100" alt="Cindy Logo" />
                     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{"Cindy"}</span>
                 </a>
                 <div class="w-auto" id="navbar-default">
                     <ul class="font-medium flex flex-col p-0 flex-row space-x-8 mt-0">
                         <li>
-                            <a href="#" class="flex items-center text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                                <img src="/gear.svg" class="h-8 mr-3" alt="Settings icon" />
+                            <a href="#" class="group flex items-center text-gray-900 md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                                <img src="/gear.svg" class="h-8 mr-3 group-hover:rotate-12 transition duration-100" alt="Settings icon" />
                                 <span class="hidden md:block p-0">
                                     {"Settings"}
                                 </span>
